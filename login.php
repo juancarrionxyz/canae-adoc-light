b0e72a6796b8506a407c332628e2aba56f954b13
Juan Carrión <6473248+jnfncm@users.noreply.github.com>
2019-10-10 23:10:26 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Canae Adoc Light login page
+ *
+ * @package canae-adoc-light
+ */
+
+require_once("./sys/main_setup.php");
+
+if (uLogged()) {
+	header("Location: " . $adoc_url);
+	die();
+}
+
+if (! empty($_POST)) {
+	$adoc_errors = array();
+	$form_email = $_POST["form_email"];
+	$form_password = $_POST["form_password"];
+
+	if (! userEmailExists($form_email)) {
+		$adoc_errors[] = "La dirección de correo electrónico introducida no existe.";
+	} else {
+		$user_data = userGetData(userIdByEmail($form_email));
+		
+		$hashed_password = generateHash($form_password, $user_data["user_password"]);
+		
+		if($hashed_password != $user_data["user_password"]) {
+			$adoc_errors[] = "La contraseña introducida es incorrecta.";
+		} else {
+			$loggedInUser = new loggedInUser($user_data);
+			$_SESSION["canae_adoc_user"] = $loggedInUser;
+			header("Location: " . $adoc_url);
+			die();
+		}
+	}
+}
+
+$adoc_pagetitle = "Iniciar sesión";
+require_once($adoc_path . "/thm/header.php");
+
+?>
+	<div>
+		<h1>Iniciar sesión</h1>
+		<form action="<?php echo $adoc_url; ?>/login.php" method="post">
+			<label for="form_email">Dirección de correo electrónico</label>
+			<input id="form_email" name="form_email" required="required" type="email">
+			<label for="form_password">Contraseña</label>
+			<input id="form_password" name="form_password" required="required" type="password">
+			<button type="submit">Iniciar sesión</button>
+		</form>
+	</div>
+<?php
+
+require_once($adoc_path . "/thm/footer.php");
+
+?>
