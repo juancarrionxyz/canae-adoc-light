8faf266bee5f4c1c309c9be18023aff1597ade22
Juan Carrión <6473248+jnfncm@users.noreply.github.com>
2019-10-11 00:08:21 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,17 @@
+<?php
 
+/**
+ * Canae Adoc Light logout page
+ *
+ * @package canae-adoc-light
+ */
+
+require_once("./sys/main_setup.php");
+
+if (uLogged()) {
+	$loggedInUser->logout();
+}
+
+header("Location: " . $adoc_url);
+
+?>
