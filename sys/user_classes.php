48ab8be48880aa4f2449d0335b350f0e034657f1
Juan Carrión <6473248+jnfncm@users.noreply.github.com>
2019-10-11 00:06:20 +0200

    Create user_classes.php

---
+++ sys/user_classes.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Canae Adoc Light user classes file
+ *
+ * @package canae-adoc-light
+ */
+
+class loggedInUser {
+	/**
+	 * User id (number)
+	 */
+	private $user_id;
+	
+	/**
+	 * User (full) name
+	 */
+	private $user_name;
+	
+	/**
+	 * User email address
+	 */
+	private $user_email;
+	
+	/**
+	 * User password (hashed)
+	 */
+	private $user_password;
+	
+	function __construct($user_data) {
+		$this->user_id = $user_data["user_id"];
+		$this->user_name = $user_data["user_name"];
+		$this->user_email = $user_data["user_email"];
+		$this->user_password = $user_data["user_password"];
+	}
+	
+	public function getUserId() {
+		return $this->user_id;
+	}
+	
+	public function getUserName() {
+		return $this->user_name;
+	}
+	
+	public function getUserEmail() {
+		return $this->user_Email;
+	}
+	
+	public function getUserPassword() {
+		return $this->user_password;
+	}
+
+	public function logout() {
+		destroySession("canae_adoc_user");
+	}
+}
+
+?>
