c1734ce3e8822ca2fd76ff184f4fa4c812ff6f78
Juan Carrión <6473248+jnfncm@users.noreply.github.com>
2019-11-06 13:11:29 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,52 +6,71 @@
  * @package canae-adoc-light
  */
 
-require_once("./sys/main_setup.php");
+require_once("./sys/main.php");
 
-if (User::isLoggedIn()) {
-	header("Location: " . $adoc_url);
-	die();
-}
+App::requireNotLogged();
 
 if (! empty($_POST)) {
 	$adoc_errors = array();
 	$form_email = $_POST["form_email"];
 	$form_password = $_POST["form_password"];
 
-	if (! User::mailExists($form_email)) {
-		$adoc_errors[] = "La dirección de correo electrónico introducida no existe.";
+	if (! User::emailExists($form_email)) {
+		$adoc_errors[] = "La dirección de correo electrónico introducida no pertenece a ningún usuario.";
 	} else {
-		$user_data = User::getData(User::idByEmail($form_email));
+		$user = User::retrieve(User::idByEmail($form_email));
 		
-		$hashed_password = generateHash($form_password, $user_data["user_password"]);
+		$hashed_password = App::generateHash($form_password, $user->getPassword());
 		
-		if($hashed_password != $user_data["user_password"]) {
+		if($hashed_password != $user->getPassword()) {
 			$adoc_errors[] = "La contraseña introducida es incorrecta.";
 		} else {
-			$loggedInUser = new loggedInUser($user_data);
-			$_SESSION["canae_adoc_user"] = $loggedInUser;
-			header("Location: " . $adoc_url);
+			$loggedInUser = $user;
+			$_SESSION["canae_adoc_logged_in_user"] = $loggedInUser;
+
+			header("Location: " . App::getUrl());
 			die();
 		}
 	}
 }
 
-$adoc_pagetitle = "Iniciar sesión";
-require_once($adoc_path . "/thm/header.php");
+App::setPageTitle("Iniciar sesión");
+App::getThemeHeader();
 
 ?>
-	<div>
-		<h1>Iniciar sesión</h1>
-		<form action="<?php echo $adoc_url; ?>/login.php" method="post">
-			<label for="form_email">Dirección de correo electrónico</label>
-			<input id="form_email" name="form_email" required="required" type="email">
-			<label for="form_password">Contraseña</label>
-			<input id="form_password" name="form_password" required="required" type="password">
-			<button type="submit">Iniciar sesión</button>
-		</form>
-	</div>
+				<div class="mdl-layout-spacer"></div>
+				<div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--4-col">
+					<div class="mdl-card__supporting-text">
+						<h3 class="adoc-login-page__app-title">Canae Adoc Light</h3>
+						<p><img src="<?php echo App::getUrl(); ?>/thm/aboutme-scene.png"></p>
+						<p>Bienvenido a la aplicación de documentación de asambleas de Canae Confederación Estatal de Asociaciones de Estudiantes, <strong>Adoc Light</strong>.</p>
+						<p>Para obtener o recuperar tus credenciales, contacta con la persona de referencia de la organización.</p>
+					</div>
+				</div>
+				<div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--4-col">
+					<div class="mdl-card__supporting-text">
+						<form action="<?php echo App::getUrl(); ?>/login.php" method="post">
+							<div class="adoc-form__row">
+								<div class="mdl-textfield mdl-js-textfield">
+									<input class="mdl-textfield__input" name="form_email" required="required" type="email" id="form_email"/>
+									<label class="mdl-textfield__label" for="form_email">Dirección de correo electrónico</label>
+								</div>
+							</div>
+							<div class="adoc-form__row">
+								<div class="mdl-textfield mdl-js-textfield">
+									<input class="mdl-textfield__input" name="form_password" required="required" type="password" id="form_password"/>
+									<label class="mdl-textfield__label" for="form_password">Contraseña</label>
+								</div>
+							</div>
+							<div class="adoc-form__row">
+								<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">Iniciar sesión</button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="mdl-layout-spacer"></div>
 <?php
 
-require_once($adoc_path . "/thm/footer.php");
+App::getThemeFooter();
 
 ?>
