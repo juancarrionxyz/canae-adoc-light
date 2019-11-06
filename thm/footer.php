54bef40a754498947c0652649a8005a6b725e3bd
Juan Carrión <6473248+jnfncm@users.noreply.github.com>
2019-11-06 13:03:55 +0100

    Update footer.php

--- thm/footer.php
+++ thm/footer.php
@@ -5,8 +5,9 @@
  * @package canae-adoc-light
  */
  
- ?>				</div>
+?>				</div>
+                		<div class="adoc-footer-text"><p>Desarrollado con <i class="material-icons">favorite</i> por <a href="https://juancarrion.xyz" title="JuanCarrion"><img src="<?php echo App::getUrl(); ?>/thm/juancarrion-logo.png" style="height: 25px; width: 53.61px;"></a> para <a href="http://canae.org" title="Canae Confederación Estatal de Asociaciones de Estudiantes"><img src="<?php echo App::getUrl(); ?>/thm/canae-logo.png" style="height: 25px;"></a>.</p></div>
 			</main>
-		</section>
+		</div>
 	</body>
 </html>
